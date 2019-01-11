5b4b6268eb3de577d65108af7af053eb40d04931
Oluwatosin Folaranmi <foluwatosin@ITILP_L014.simplexsystems.com>
2019-01-11 13:18:10 +0100

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Data Visualisation with Vue.js and Chart.js"/>
+    <visualisation></visualisation>
   </div>
 </template>
 
 <script>
+import Visualisation from './components/Visualisation.vue';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
+    Visualisation,
     HelloWorld
   }
 }
---
+++ src/components/Visualisation.vue
@@ -0,0 +1,62 @@
+<template>
+    <chart :chart-data="datacollection"></chart>
+</template>
+
+<script>
+    import Chart from "./../Chart.js";
+    export default {
+        components: {
+            Chart
+        },
+        data() {
+            return {
+                datacollection: null
+            };
+        },
+        mounted() {
+            this.fillData();
+        },
+        methods: {
+            fillData() {
+                this.datacollection = {
+                    labels: [
+                        "January",
+                        "February",
+                        "March",
+                        "April",
+                        "May",
+                        "June",
+                        "July",
+                        "August",
+                        "September",
+                        "October",
+                        "November",
+                        "December"
+                    ],
+                    datasets: [
+                        {
+                            label: "Git Commits",
+                            backgroundColor: "#f87979",
+                            data: [70, 20, 12, 39, 100, 40, 95, 80, 80, 20, 12, 101]
+                        },
+                        {
+                            label: "Monthly incomes",
+                            backgroundColor: "#A5CC82",
+                            data: [205, 408, 188, 190, 58, 200, 190, 400, 164, 254, 290, 201]
+                        },
+                         {
+                            label: "Monthly Summary",
+                            backgroundColor: "#B00C82",
+                            data: [105, 208, 198, 100, 190, 150, 164, 154, 290, 201]
+                        }
+
+                    ]
+                };
+            },
+        }
+    };
+</script>
+
+<style>
+
+</style>
